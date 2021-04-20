e667f92e41929372b34a76ca8e5f47a7b9906f89
Daniel <danielmuest@gmail.com>
2021-04-20 22:56:03 +0200

    Primer commit todo funcionando bien

---
+++ js/prueba.js
@@ -0,0 +1,39 @@
+let weather = {
+    "ApiKey" : "8956af9be42e5f981ce6222a1a1e56d2",
+    fetchWeather: function (city) {
+        fetch(
+            "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + this.ApiKey
+        ).then((response) => response.json())
+        .then((data) => this.diplayWeather(data));
+    },
+    diplayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, temp_min, temp_max, humidity } = data.main;
+        //const { speed } = data.wind;
+        document.querySelector(".datosbuscadosCiudad").innerText = name;
+        document.querySelector(".iconotiempo").src = "https://openweathermap.org/img/wn/" + icon + "@4x.png";
+        document.querySelector(".gradosennumero").innerText = (temp-273.15).toFixed() + "º";
+        document.querySelector(".datosbuscadosPais").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".descripciontiempo").innerText = description;
+        document.querySelector(".temperaturamaxima").innerText = (temp_max-273.15).toFixed() + "º" ;
+        document.querySelector(".temperaturaminima").innerText = (temp_min-273.15).toFixed() + "º";
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".ciudadabuscar").value);
+    }
+};
+
+document
+.querySelector(".botonenviar")
+.addEventListener("click", function () {
+    weather.search();
+});
+
+document
+.querySelector(".ciudadabuscar")
+.addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+});
